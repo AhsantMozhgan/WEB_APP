a96d29c33270091048c07ad1c531b1ae6bed6a76
AhsantMozhgan <mozhganahsant@gmail.com>
2026-02-26 15:06:03 -0800

    adding-event-listener

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,5 +1,16 @@
 let backdrop = document.querySelector('.backdrop')
-
+let modal = document.querySelector('.modal')
 // console.log(backdrop)
+// backdrop.style.display = "block"
 
-backdrop.style.display = "block"
+let selectPlanBtn = document.querySelectorAll('.plan button')
+
+// console.log(selectPlanBtn)
+
+for (let i = 0; i < selectPlanBtn.length; i++) {
+    // selectPlanBtn[i].addEventListener('click', function() {
+    selectPlanBtn[i].addEventListener('click', () => {
+        modal.style.display = 'block'
+        backdrop.style.display = 'block'
+    })
+}
