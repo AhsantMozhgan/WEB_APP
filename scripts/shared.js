c02f10ac58a92d2280392ae77483fc058690c162
AhsantMozhgan <mozhganahsant@gmail.com>
2026-02-26 15:19:49 -0800

    modal-close-button

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,16 +1,28 @@
+// console.log('Hello word')
 let backdrop = document.querySelector('.backdrop')
-let modal = document.querySelector('.modal')
 // console.log(backdrop)
-// backdrop.style.display = "block"
+let modal = document.querySelector('.modal')
 
 let selectPlanBtn = document.querySelectorAll('.plan button')
-
 // console.log(selectPlanBtn)
+let modalClosBtn = document.querySelector('.modal__action—-negative')
+
+
+// for (let i = 0; i < selectPlanBtn.length; i++) {
+//     selectPlanBtn[i].addEventListener('click', function() {
+
+//     })
+// }
 
 for (let i = 0; i < selectPlanBtn.length; i++) {
-    // selectPlanBtn[i].addEventListener('click', function() {
     selectPlanBtn[i].addEventListener('click', () => {
         modal.style.display = 'block'
-        backdrop.style.display = 'block'
     })
+}
+
+backdrop.addEventListener('click', closeModel)
+modalClosBtn.addEventListener('click', closeModel)
+function closeModel() {
+    backdrop.style.display = 'none'
+    modal.style.display = 'none'
 }
